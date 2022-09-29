d4b7a9ab45c0d7a0773ee4951ef3cb8811d971d9
mohammed <togachivon@gmail.com>
2022-09-29 17:30:28 +0100

    fix_errors

--- Assets/scripts/readtrigger.cs
+++ Assets/scripts/readtrigger.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class readtrigger : MonoBehaviour
 {
     public Transform reodprifabe;
+    bool is_insade;
+
+    private void Awake()
+    {
+        is_insade = false;
+    }
+
+
     void OnTriggerEnter(Collider character)
     {
         
-        if (character.gameObject.tag == "Player" )
+        if (character.gameObject.tag == "Player" & is_insade == false )
         {
-           Instantiate(reodprifabe, new Vector3(0, 0, transform.parent.position.z + 262.4f), reodprifabe.rotation);
+            is_insade = true;
+            Instantiate(reodprifabe, new Vector3(0, 0, transform.parent.position.z + 262.4f), reodprifabe.rotation);
             
         }
     }
