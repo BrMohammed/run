bfe3f3ed284586bdbb49bee0cd1babbb061b62e4
mohammed <togachivon@gmail.com>
2022-09-30 18:07:26 +0100

    fix_size_of_icons

--- Temp/gradleOut/launcher/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/com/voon/run/R.java
+++ Temp/gradleOut/launcher/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/com/voon/run/R.java
@@ -438,7 +438,6 @@ public final class R {
     public static final int app_icon=0x7f090000;
     public static final int app_icon_round=0x7f090001;
     public static final int ic_launcher_background=0x7f090002;
-    public static final int ic_launcher_foreground=0x7f090003;
   }
   public static final class string {
     public static final int app_name=0x7f0a0000;
